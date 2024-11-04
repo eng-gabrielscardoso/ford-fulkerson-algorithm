e9e94ec8aa9625409006a04ab990e505d7665575
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-11-04 15:42:56 -0300

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use core::{edge::Edge, graph::Graph, vertex::Vertex};
+
+mod core;
+
 fn main() {
-    println!("Hello, world!");
+    let mut graph = Graph::new();
+
+    let belem = Vertex::new("Belém");
+    let barcarena = Vertex::new("Barcarena");
+    let ananindeua = Vertex::new("Ananindeua");
+
+    graph.add_vertex(belem.clone());
+    graph.add_vertex(barcarena.clone());
+    graph.add_vertex(ananindeua.clone());
+
+    graph.add_edge(Edge::new(&barcarena, &belem, 10));
+    graph.add_edge(Edge::new(&ananindeua, &belem, 15));
+    graph.add_edge(Edge::new(&barcarena, &ananindeua, 20));
+
+    let max_flow = graph.ford_fulkerson("Barcarena", "Belém");
+
+    println!("Fluxo máximo de entregas: {}", max_flow);
 }
